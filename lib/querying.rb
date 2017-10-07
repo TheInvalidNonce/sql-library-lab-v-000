2dbc3322cb30802ce8206a085ec8d8a9c54ff64b
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-07 22:03:03 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,12 +1,12 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year 
+  "SELECT title, year
   FROM books
   WHERE series_id = 1
   ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto 
+  "SELECT name, motto
   FROM characters
   ORDER BY length(motto) DESC
   LIMIT 1;"
@@ -14,7 +14,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) 
+  "SELECT species, COUNT(species)
   FROM characters
   GROUP BY species
   ORDER BY COUNT(species) DESC
@@ -22,7 +22,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name 
+  "SELECT authors.name, subgenres.name
   FROM authors
   INNER JOIN series
   ON author_id = authors.id
@@ -31,7 +31,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title 
+  "SELECT title
   FROM series
   INNER JOIN characters
   WHERE species = 'human'
@@ -39,7 +39,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT name, COUNT(name) 
+  "SELECT name, COUNT(name)
   FROM character_books
   INNER JOIN characters
   ON character_id = characters.id
